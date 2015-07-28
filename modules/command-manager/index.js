9fdc5c28249ca5fcde2d7c2931fb88ecd02777c7
iksoiks <christian.cricri94@gmail.com>
2015-07-28 20:40:24 +0200

    First commit

--- modules/command-manager/index.js
+++ modules/command-manager/index.js
@@ -1,1 +1,11 @@
+var commandManager = module.exports = {};
 
+/**
+ * Routes commands with callbacks
+ * @param command The callback was triggered when a user send this string.
+ * @param cb The callback function
+ * @returns {*}
+ */
+commandManager.on = function (command, cb) {
+    return null
+};
