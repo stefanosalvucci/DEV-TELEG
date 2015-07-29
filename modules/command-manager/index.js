995fab9dc6569d9e7652f25c360404a08f6249ee
lorenzo-pizzari <lor.pizzari@stud.uniroma3.it>
2015-07-30 00:23:06 +0200

    added node-telegram-bot-api && started command-manager

--- modules/command-manager/index.js
+++ modules/command-manager/index.js
@@ -1,11 +1,1 @@
-var commandManager = module.exports = {};
-
-/**
- * Routes commands with callbacks
- * @param command The callback was triggered when a user send this string.
- * @param cb The callback function
- * @returns {*}
- */
-commandManager.on = function (command, cb) {
-    return null
-};
+module.exports = require('./src/commandManager');
