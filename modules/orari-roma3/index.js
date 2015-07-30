fd177bb1e7dbc6839fe18b269f386cde57624130
iksoiks <christian.cricri94@gmail.com>
2015-07-30 16:02:17 +0200

    Develop of orari-roma3 started

---
+++ modules/orari-roma3/index.js
@@ -0,0 +1,12 @@
+var OrariRomaTre = function () {
+};
+
+/**
+ * Update the local database with data fetched from orari.uniroma.it/ing/esporta.php
+ * TODO
+ */
+OrariRomaTre.prototype.updateDb = function () {
+    // Please read https://github.com/Leonidas-from-XIV/node-xml2js
+};
+
+module.exports = new OrariRomaTre();
