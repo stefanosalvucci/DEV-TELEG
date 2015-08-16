aeeabf02eb3fc7232c39066e91c651c78f1f193f
iksoiks <christian.cricri94@gmail.com>
2015-08-16 17:26:12 +0200

    Dipartimenti

--- modules/database.js
+++ modules/database.js
@@ -29,7 +29,7 @@ module.exports = {
             MongoClient.connect(mongoDbUrl, {promiseLibrary: Promise}, function (err, database) {
                 if (err) return reject(err);
                 db = database;
-                console.log("Db connected");
+                console.log('Db connected');
                 resolve(db);
             });
         });
---
+++ modules/dipartimenti.js
@@ -0,0 +1,46 @@
+module.exports = {
+    ARCHITETTURA: {
+        id: 1,
+        orariKey: 'arch'
+    },
+    ECONOMIA: {
+        id: 2,
+        orariKey: 'economia'
+    },
+    FILOSOFIA: {
+        id: 3
+    },
+    GIURISPRUDENZA: {
+        id: 4,
+        orariKey: 'giur'
+    },
+    INGEGNERIA: {
+        id: 5,
+        orariKey: 'ing'
+    },
+    LINGUE: {
+        id: 6,
+        orariKey: 'lettere'
+    },
+    MATEMATICA_FISICA: {
+        id: 7
+    },
+    SCIENZE: {
+        id: 8,
+        orariKey: 'smfn'
+    },
+    SCIENZE_FORMAZIONE: {
+        id: 9,
+        orariKey: 'formazione'
+    },
+    SCIENZE_POLITICHE: {
+        id: 10,
+        orariKey: 'spolitiche'
+    },
+    STUDI_AZIENDALI: {
+        id: 11
+    },
+    STUDI_UMANISTICI: {
+        id: 12
+    }
+};
