514b573d473d40ab080bf55d4639d8b707cf58ae
iksoiks <christian.cricri94@gmail.com>
2015-10-06 12:22:38 +0200

    italian answers

--- README.md
+++ README.md
@@ -1,4 +1,59 @@
 # LUG Roma Tre Bot
 
+This software is a Telegram Bot that provides informations about "Università di Roma Tre".
+
 ## Version
-0.0.1 alpha
+0.1.0 beta
+
+## Dependencies
+* mongodb
+* nodejs
+* npm
+
+## Installation
+1. setup dependencies;
+2. clone this repository;
+3. like others node based software, run `npm install` into the cloned folder;
+4. create a new Telegram Bot like described in this page: https://core.telegram.org/bots#3-how-do-i-create-a-bot
+5. copy `config.json.sample` to `config.json` and edit it using your personal informations.
+
+## Configuration
+If the software is configurable, describe it in detail, either here or in other documentation to which you link.
+
+## Usage
+Run `node bin/bot`
+
+## Known issues
+This software is in beta release and it contain much issues.
+
+## Getting help
+Instruct users how to get help with this software; this might include links to an issue tracker, wiki, mailing list, etc.
+
+**Example**
+
+If you have questions, concerns, bug reports, etc, please file an issue in this repository's Issue Tracker.
+
+## Getting involved
+
+This section should detail why people should get involved and describe key areas you are
+currently focusing on; e.g., trying to get feedback on features, fixing certain bugs, building
+important pieces, etc.
+
+General instructions on _how_ to contribute should be stated with a link to [CONTRIBUTING](CONTRIBUTING.md).
+
+
+----
+
+## Open source licensing info
+1. [TERMS](TERMS.md)
+2. [LICENSE](LICENSE)
+3. [CFPB Source Code Policy](https://github.com/cfpb/source-code-policy/)
+
+
+----
+
+## Credits and references
+
+1. Projects that inspired you
+2. Related projects
+3. Books, papers, talks, or other sources that have meaningful impact or influence on this project
--- commands/index.js
+++ commands/index.js
@@ -9,14 +9,17 @@ var handleError = function (err, msg, telegramBot) {
     console.error(err.stack);
 };
 
+var listaComandi = '/aulelibere - Mostra le aule libere adesso' +
+    '\n/help - Mostra la lista dei comandi disponibili';
+
 commands.on('/start', function (msg, telegramBot) {
-    telegramBot.sendMessage(msg.chat.id, 'Che vuoi?');
+    telegramBot.sendMessage(msg.chat.id, 'Ciao! Di cosa hai bisogno?' +
+        '\n' + listaComandi);
 });
 
 commands.on('/help', function (msg, telegramBot) {
-    telegramBot.sendMessage(msg.chat.id, 'Eccoti qualche comando "sicuramente" funzionante:\n' +
-        '\n/aulelibere - Mostra le aule libere adesso' +
-        '\n/help - C\'è bisogno di spiegazioni?');
+    telegramBot.sendMessage(msg.chat.id, 'Ecco la lista delle cose che puoi chiedermi:\n' +
+        '\n' + listaComandi);
 });
 
 commands.on('/aulelibere', function (msg, telegramBot) {
@@ -24,7 +27,7 @@ commands.on('/aulelibere', function (msg, telegramBot) {
     user.getDipartimento().then(function (dipartimentoId) {
         return orari.getAuleLibere(dipartimenti[dipartimentoId]);
     }).then(function (aule) {
-        var message = 'Eccoti una lista delle aule libere (sperando non siano chiuse):';
+        var message = 'Eccoti una lista delle aule libere (sperando non siano chiuse!):';
         aule.forEach(function (item) {
             message += '\n - ' + item.aula;
             if (item.date.getDate() == new Date().getDate())
@@ -37,12 +40,12 @@ commands.on('/aulelibere', function (msg, telegramBot) {
 });
 
 commands.on('/lezioni', function (msg, telegramBot) {
-    telegramBot.sendMessage(msg.chat.id, 'Mi dispiace, ma gli scansafatiche del LUG Roma 3 ancora non mi hanno' +
+    telegramBot.sendMessage(msg.chat.id, 'Mi dispiace, ma gli scansafatiche del LUG Roma Tre ancora non mi hanno' +
         ' insegnato come scrivere le lezioni odierne!');
 });
 
 commands.on('/cometichiami', function (msg, telegramBot) {
-    telegramBot.sendMessage(msg.chat.id, 'Mi chiamo LUG Roma 3 Bot!');
+    telegramBot.sendMessage(msg.chat.id, 'Mi chiamo Info Roma Tre Bot!');
 });
 
 commands.on('/grazie', function (msg, telegramBot) {
@@ -50,12 +53,32 @@ commands.on('/grazie', function (msg, telegramBot) {
 });
 
 // TODO Access this command only in debug mode
-commands.on('/debug', function (msg, telegramBot) {
-    console.log(msg);
-    var user = new User(msg.from.id);
-    user.getDipartimento();
-});
+//commands.on('/debug', function (msg, telegramBot) {
+//    console.log(msg);
+//    var user = new User(msg.from.id);
+//    user.getDipartimento();
+//});
 
 commands.on('/default', function (msg, telegramBot) {
-    telegramBot.sendMessage(msg.chat.id, 'Non ho capito!');
+    var message = '';
+    var rand = Math.floor(Math.random() * 5);
+    switch (rand) {
+        case 0:
+            message = 'Mi dispiace ma non ho capito!';
+            break;
+        case 1:
+            message = 'Hey, non sono mica così intelligente!';
+            break;
+        case 2:
+            message = 'Ma tu non hai fame?';
+            break;
+        case 3:
+            message = "\"Software is like sex: it's better when it's free.\" - Linus Torvalds";
+            break;
+        case 4:
+            message = "Scusami... Ma non so proprio cosa dirti!";
+            break;
+    }
+    message += '\n\nDigita /help per la lista dei comandi disponibili!';
+    telegramBot.sendMessage(msg.chat.id, message);
 });
