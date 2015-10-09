1259cb695bb1797b696ed42f2202a9375dea294a
Christian Micocci <christian.cricri94@gmail.com>
2015-10-09 13:28:29 +0200

    Fixes #2: typo

--- modules/dipartimenti.js
+++ modules/dipartimenti.js
@@ -15,7 +15,7 @@ module.exports = [
     },
     {
         id: 3,
-        name: 'Filosofia, Comunicazione e Spettacoloa'
+        name: 'Filosofia, Comunicazione e Spettacolo'
     },
     {
         id: 4,
